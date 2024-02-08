373e4bf625af10f6349222d4a52ec85842d2b2a2
lagger543 <@gmail.com>
2024-02-08 12:52:29 +0100

    Add @Component annotation

--- src/main/java/cma/api/mapper/CMAMapper.java
+++ src/main/java/cma/api/mapper/CMAMapper.java
@@ -2,9 +2,9 @@ package cma.api.mapper;
 
 import cma.api.model.Contact;
 import cma.api.dto.ContactDTO;
-
+import org.springframework.stereotype.Component;
+@Component
 public class CMAMapper {
-
     public ContactDTO convertAnEntityToDTO(Contact contact) {
         return new ContactDTO(contact.getId(),contact.getFirstName(),contact.getLastName(),contact.getDateOfBirth(),contact.getAddress(),contact.getMobileNumber());
     }
