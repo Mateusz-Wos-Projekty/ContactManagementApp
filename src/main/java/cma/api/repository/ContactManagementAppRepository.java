70efcace94d8de00b64e44e2383f659c30c345c4
lagger543 <@gmail.com>
2024-02-08 13:08:14 +0100

    Remove redundant unnecessary empty line

--- src/main/java/cma/api/repository/ContactManagementAppRepository.java
+++ src/main/java/cma/api/repository/ContactManagementAppRepository.java
@@ -2,9 +2,7 @@ package cma.api.repository;
 
 import cma.api.model.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
-
 
 @Repository
 public interface ContactManagementAppRepository extends JpaRepository<Contact,Integer> {
