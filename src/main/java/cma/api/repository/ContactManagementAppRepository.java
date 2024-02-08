13f5e6c13050d3d9acddaf82783323c3ab702b9b
lagger543 <@gmail.com>
2024-02-08 13:04:59 +0100

    Remove redundant import statement

--- src/main/java/cma/api/repository/ContactManagementAppRepository.java
+++ src/main/java/cma/api/repository/ContactManagementAppRepository.java
@@ -2,7 +2,9 @@ package cma.api.repository;
 
 import cma.api.model.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface ContactManagementAppRepository extends JpaRepository<Contact,Integer> {
