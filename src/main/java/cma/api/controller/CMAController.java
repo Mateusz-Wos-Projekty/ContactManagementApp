dfb207b1dbf0a2cd2b57df2812f96fb5948ffd10
p2501980 <60095586+p2501980@users.noreply.github.com>
2024-02-09 17:35:29 +0100

    Add @GetMapping functionality for the CMA Controller
    The CMA controller has two new methods added which allow for the retrieval of a single or multiple objects from the DB.

--- src/main/java/cma/api/controller/CMAController.java
+++ src/main/java/cma/api/controller/CMAController.java
@@ -9,12 +9,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.rmi.ServerException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class CMAController {
     private final  CMAService contactService;
     private final CMAMapper cmaMapper;
-
     @Autowired
     public CMAController(CMAService contactService,CMAMapper cmaMapper) {
         this.contactService = contactService;
@@ -24,6 +25,32 @@ public class CMAController {
     public String getHelloWorld(@RequestParam(value = "name", defaultValue = "Hello World!") String name ) {
         return name;
     }
+    @GetMapping("/contact/{id}")
+    public ResponseEntity<Contact> getContact(@PathVariable("id") Integer id) {
+
+        if(contactService.getContactById(id).getId() == null){
+
+            Contact newEmptyContact = new Contact();
+            return new ResponseEntity<>(newEmptyContact, HttpStatus.OK);
+
+        }else{
+            return  new ResponseEntity<>(contactService.getContactById(id), HttpStatus.OK);
+        }
+    }
+
+    @GetMapping("/contacts")
+    public ResponseEntity<List<Contact>> getContacts() {
+
+        if(contactService.getContacts() == null){
+
+            List<Contact> newEmptyContactList = new ArrayList<>();
+            return new ResponseEntity<>(newEmptyContactList, HttpStatus.OK);
+
+        }else{
+            return  new ResponseEntity<>(contactService.getContacts(), HttpStatus.OK);
+        }
+    }
+
     /*skupiam sie narazie na post mapping gdy będę mieć już działającą werjsę,
       przejdę do pozostałych endpointow */
     @PostMapping("/contacts")
