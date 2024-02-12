bf7c9816ebe0d7825ffd92c83931ef0c7b863ac5
p2501980 <60095586+p2501980@users.noreply.github.com>
2024-02-12 16:54:17 +0100

    Add JpaSpecificationExecutor<Contact> to ContactManagementAppRepository class

--- src/main/java/cma/api/repository/ContactManagementAppRepository.java
+++ src/main/java/cma/api/repository/ContactManagementAppRepository.java
@@ -2,9 +2,10 @@ package cma.api.repository;
 
 import cma.api.model.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ContactManagementAppRepository extends JpaRepository<Contact,Integer> {
+public interface ContactManagementAppRepository extends JpaRepository<Contact,Integer>, JpaSpecificationExecutor<Contact> {
 
 }
