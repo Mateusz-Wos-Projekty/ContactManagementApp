9131d26033bc54cfe9a8527047b2c21680e66c5c
p2501980 <60095586+p2501980@users.noreply.github.com>
2024-02-22 16:19:25 +0100

    Modify CMAMapper by adding conversion methods

--- src/main/java/cma/api/mapper/CMAMapper.java
+++ src/main/java/cma/api/mapper/CMAMapper.java
@@ -1,8 +1,14 @@
 package cma.api.mapper;
 
+import cma.api.dto.CreateContactDTO;
+import cma.api.dto.ReturnContactDTO;
 import cma.api.model.Contact;
 import cma.api.dto.ContactDTO;
+import org.hibernate.mapping.List;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
 @Component
 public class CMAMapper {
     public ContactDTO convertAnEntityToDTO(Contact contact) {
@@ -12,4 +18,34 @@ public class CMAMapper {
 
         return new Contact(contactDTO.getId(),contactDTO.getFirstName(),contactDTO.getLastName(),contactDTO.getDateOfBirth(),contactDTO.getAddress(),contactDTO.getMobileNumber());
     }
+    public ReturnContactDTO convertCreateContactDTOToReturnContactDTO(CreateContactDTO createContactDTO){
+
+        return new ReturnContactDTO(createContactDTO.getId(),createContactDTO.getFirstName(),createContactDTO.getLastName(),createContactDTO.getDateOfBirth(),createContactDTO.getAddress(),createContactDTO.getMobileNumber());
+
+    }
+    public Contact convertCreateContactDTOToAnEntity(CreateContactDTO createContactDTO){
+
+        return new Contact(createContactDTO.getId(),createContactDTO.getFirstName(),createContactDTO.getLastName(),createContactDTO.getDateOfBirth(),createContactDTO.getAddress(),createContactDTO.getMobileNumber());
+
+    }
+    public ReturnContactDTO convertAnEntityToReturnContactDTO(Contact contact) {
+        return new ReturnContactDTO(contact.getId(),contact.getFirstName(),contact.getLastName(),contact.getDateOfBirth(),contact.getAddress(),contact.getMobileNumber());
+    }
+
+    public java.util.List<ReturnContactDTO> convertAnEntityListToReturnContactDTOList(java.util.List<Contact> contact) {
+
+        java.util.List<ReturnContactDTO> newList = new ArrayList<>() ;
+
+        for (int i = 0; i < contact.size(); i++) {
+            Contact contact1 = contact.get(i);
+
+            ReturnContactDTO newReturnContactDTO = convertAnEntityToReturnContactDTO(contact1);
+
+            newList.add(newReturnContactDTO);
+
+        }
+
+        return newList;
+    }
+
 }
