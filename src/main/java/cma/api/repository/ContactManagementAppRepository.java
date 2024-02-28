e7b91a06fcddf53a56566b687fde58ebd69794bf
p2501980 <60095586+p2501980@users.noreply.github.com>
2024-02-28 15:38:34 +0100

    Make changes to ContactManagementAppRepository

--- src/main/java/cma/api/repository/ContactManagementAppRepository.java
+++ src/main/java/cma/api/repository/ContactManagementAppRepository.java
@@ -4,11 +4,12 @@ import cma.api.model.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
 @Repository
 public interface ContactManagementAppRepository extends JpaRepository<Contact, Integer>, JpaSpecificationExecutor<Contact> {
-    List<Contact> firstNameAndLastName(String firstName, String lastname);
-    List<Contact> mobileNumber(Integer mobileNumber);
+    List<Contact> findByFirstNameAndLastName(String firstName, String lastname);
+    List<Contact> findByFirstName(String firstName);
+    List<Contact> findByLastName(String lastName);
+    List<Contact> findBy();
 }
