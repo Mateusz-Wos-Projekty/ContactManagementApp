b52801e1435070785e73295e24fe1159364b6352
Mateusz Wos <Mateusz.Wos@gds.ey.com>
2024-03-06 17:02:57 +0100

    Make changes to ContactManagementAppRepository

--- src/main/java/cma/api/repository/ContactManagementAppRepository.java
+++ src/main/java/cma/api/repository/ContactManagementAppRepository.java
@@ -1,15 +1,18 @@
 package cma.api.repository;
 
 import cma.api.model.Contact;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.query.FluentQuery;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import java.util.function.Function;
 
 @Repository
 public interface ContactManagementAppRepository extends JpaRepository<Contact, Integer>, JpaSpecificationExecutor<Contact> {
     List<Contact> findByFirstNameAndLastName(String firstName, String lastname);
     List<Contact> findByFirstName(String firstName);
     List<Contact> findByLastName(String lastName);
-    List<Contact> findBy();
+
 }
