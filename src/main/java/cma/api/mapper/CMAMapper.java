40d6ce07967540547a831bf40cbb90a1906582be
Mateusz Wos <Mateusz.Wos@gds.ey.com>
2024-03-07 12:29:54 +0100

    Indent the code in CMAMapper

--- src/main/java/cma/api/mapper/CMAMapper.java
+++ src/main/java/cma/api/mapper/CMAMapper.java
@@ -14,18 +14,23 @@ public class CMAMapper {
     public ContactDTO convertAnEntityToDTO(Contact contact) {
         return new ContactDTO(contact.getId(), contact.getFirstName(), contact.getLastName(), contact.getDateOfBirth(), contact.getAddress(), contact.getMobileNumber());
     }
+
     public Contact convertDTOToAnEntity(ContactDTO contactDTO) {
         return new Contact(contactDTO.getId(), contactDTO.getFirstName(), contactDTO.getLastName(), contactDTO.getDateOfBirth(), contactDTO.getAddress(), contactDTO.getMobileNumber());
     }
+
     public ReturnContactDTO convertCreateContactDTOToReturnContactDTO(CreateContactDTO createContactDTO) {
         return new ReturnContactDTO(createContactDTO.getId(), createContactDTO.getFirstName(), createContactDTO.getLastName(), createContactDTO.getDateOfBirth(), createContactDTO.getAddress(), createContactDTO.getMobileNumber());
     }
+
     public Contact convertCreateContactDTOToAnEntity(CreateContactDTO createContactDTO) {
         return new Contact(createContactDTO.getId(), createContactDTO.getFirstName(), createContactDTO.getLastName(), createContactDTO.getDateOfBirth(), createContactDTO.getAddress(), createContactDTO.getMobileNumber());
     }
+
     public ReturnContactDTO convertAnEntityToReturnContactDTO(Contact contact) {
         return new ReturnContactDTO(contact.getId(), contact.getFirstName(), contact.getLastName(), contact.getDateOfBirth(), contact.getAddress(), contact.getMobileNumber());
     }
+
     public List<ReturnContactDTO> convertAnEntityListToReturnContactDTOList(List<Contact> listOfContacts) {
 
         List<ReturnContactDTO> returnContactDtoList = new ArrayList<>();
