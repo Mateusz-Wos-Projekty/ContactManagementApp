85d0638e02e561a55b33d16be88716ad21e8ec41
Mateusz Wos <Mateusz.Wos@gds.ey.com>
2024-03-07 12:39:05 +0100

    Indent the code in DemoApplication tests class

--- src/test/java/com/example/cma/DemoApplicationTests.java
+++ src/test/java/com/example/cma/DemoApplicationTests.java
@@ -10,28 +10,30 @@ import java.time.LocalDate;
 
 @SpringBootTest(classes = {Contact.class, ContactDTO.class})
 class DemoApplicationTests {
-	@Test
-	public void CMAMapperDTOToEntityConversionTest() {
-		CMAMapper newCMAMapper = new CMAMapper();
+    @Test
+    public void CMAMapperDTOToEntityConversionTest() {
+        CMAMapper newCMAMapper = new CMAMapper();
 
-		ContactDTO contactDTO = new ContactDTO(10,"Mateusz","Woś", LocalDate.of(1996,12,8),"Chorzowska50",111555444);
-		Contact contact = newCMAMapper.convertDTOToAnEntity(contactDTO);
+        ContactDTO contactDTO = new ContactDTO(10, "Mateusz", "Woś", LocalDate.of(1996, 12, 8), "Chorzowska50", 111555444);
+        Contact contact = newCMAMapper.convertDTOToAnEntity(contactDTO);
 
-		assertEquals(contactDTO.getLastName(), contact.getLastName() );
-	}
-	@Test
-	public void CMAMapperEntityToDTOConversionTest() {
-		CMAMapper newCMAMapper = new CMAMapper();
+        assertEquals(contactDTO.getLastName(), contact.getLastName());
+    }
 
-		Contact contact = new Contact();
-		contact.setId(10);
-		contact.setFirstName("Marcin");
-		contact.setLastName("Mandla");
+    @Test
+    public void CMAMapperEntityToDTOConversionTest() {
+        CMAMapper newCMAMapper = new CMAMapper();
 
-		ContactDTO newContactDTO = newCMAMapper.convertAnEntityToDTO(contact);
+        Contact contact = new Contact();
+        contact.setId(10);
+        contact.setFirstName("Marcin");
+        contact.setLastName("Mandla");
 
-		assertEquals(contact.getLastName(), newContactDTO.getLastName());
-	}
-	private void assertEquals(String firstName, String firstName1) {
-	}
+        ContactDTO newContactDTO = newCMAMapper.convertAnEntityToDTO(contact);
+
+        assertEquals(contact.getLastName(), newContactDTO.getLastName());
+    }
+
+    private void assertEquals(String firstName, String firstName1) {
+    }
 }
