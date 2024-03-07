f65ed7e44b86fcc4ed259ba791e28127566d8dae
Mateusz Wos <Mateusz.Wos@gds.ey.com>
2024-03-07 12:32:59 +0100

    Indent the code in Contact class

--- src/main/java/cma/api/model/Contact.java
+++ src/main/java/cma/api/model/Contact.java
@@ -7,10 +7,10 @@ import java.time.LocalDate;
 @NamedQuery(name = "findByFirstName", query = "from Contact u where u.firstName = :firstName")
 @NamedQuery(name = "findByLastName", query = "from Contact u where u.lastName = :lastName")
 @NamedQuery(name = "findByFirstNameAndLastName", query = "from Contact u where u.firstName = :firstName and u.lastName = :lastName")
-@Table(name= "CONTACT")
+@Table(name = "CONTACT")
 public class Contact {
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Integer id;
     @Column(name = "firstName")
@@ -32,6 +32,7 @@ public class Contact {
         address = "";
         mobileNumber = 0;
     }
+
     public Contact(Integer id, String firstName, String lastName, LocalDate dateOfBirth, String address, Integer mobileNumber) {
         this.id = id;
         this.firstName = firstName;
@@ -40,42 +41,55 @@ public class Contact {
         this.address = address;
         this.mobileNumber = mobileNumber;
     }
+
     public void setId(Integer id) {
         this.id = id;
     }
+
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
     public void setDateOfBirth(LocalDate dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
+
     public void setAddress(String address) {
         this.address = address;
     }
+
     public void setMobileNumber(Integer mobileNumber) {
         this.mobileNumber = mobileNumber;
     }
+
     public Integer getId() {
         return id;
     }
+
     public String getFirstName() {
         return firstName;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public LocalDate getDateOfBirth() {
         return dateOfBirth;
     }
+
     public String getAddress() {
         return address;
     }
+
     public Integer getMobileNumber() {
         return mobileNumber;
     }
+
     @Override
     public String toString() {
         return "Contact{" +
